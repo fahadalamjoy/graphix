640522bbc8d09f1502ac0f952834711586629f0b
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2023-09-14 14:34:01 +0600

    updated

--- app/showskill/page.jsx
+++ app/showskill/page.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 export default function page() {
   return (
     <section className='container  lg:py-12 py-6 lg:h-[90vh] h-[100vh] w-full bg-black relative text-white font-custom items-center'>
-        <div className='flex lg:flex-row flex-col gap-12 py-12 relative'>
-            <div className='mb-6 absolute lg:top-[-2rem] top-[-8rem] inset-0 left-[2rem] lg:my-0 my-6'><h1 className='text-2xl mb-6'>Branding and Identity Design</h1></div>
+        <div className='flex lg:flex-row flex-col gap-12 py-12 relative '>
+            <div className='mb-6 absolute lg:top-[-2rem] top-[-5rem] inset-0 left-[2rem] lg:my-0 my-6'><h1 className='text-2xl mb-6'>Branding and Identity Design</h1></div>
             <div className='absolute  inset-0 flex items-center lg:text-start text-center justify-center lg:my-0 my-6'><h1 className='text-2xl'>Illustrations and Infographics</h1></div>
             <div className='absolute lg:top-[-10rem] top-[6rem] right-[2rem] inset-0 flex items-center lg:justify-end justify-center lg:my-0 my-6'><h1 className='text-2xl'>Logo Design</h1></div>
         </div>
